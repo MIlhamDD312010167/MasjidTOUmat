66266429bb403e8c297a2fd0f3016b0706c99f2f
Muhammad Ilham Dwi Dharmawan <106169328+MIlhamDD312010167@users.noreply.github.com>
2023-05-28 11:35:32 +0700

    Create README.md

---
+++ app/src/main/java/com/example/masjidtoumat/Infaq.java
@@ -0,0 +1,14 @@
+package com.example.masjidtoumat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Infaq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infaq);
+    }
+}
---
+++ app/src/main/java/com/example/masjidtoumat/Shodaqoh.java
@@ -0,0 +1,13 @@
+package com.example.masjidtoumat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shodaqoh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shodaqoh);
+    }
+}
